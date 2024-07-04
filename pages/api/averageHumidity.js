fd3605af659676c09e63ce8c9bdedb928084d256
AlifBrahim <m_alif_muhammad@soc.uum.edu.my>
2024-07-05 02:24:01 +0800

    Convert timezone

--- pages/api/averageHumidity.js
+++ pages/api/averageHumidity.js
@@ -23,7 +23,8 @@ export default async function handler(req, res) {
     const [rows] = await connection.execute(`
     SELECT AVG(humidity) as avgHumidity 
     FROM environment_data 
-    WHERE timestamp >= ? AND timestamp <= ?`,
+    WHERE CONVERT_TZ(timestamp, '+08:00', '+00:00') >= ? 
+      AND CONVERT_TZ(timestamp, '+08:00', '+00:00') <= ?`,
         [thirtyMinutesAgoString, nowString]
     );
 
